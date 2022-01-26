e8754d5a7934817aa8dbe848a953d1db439d0a30
handaniel <danielhandsantiago@gmail.com>
2022-01-25 21:41:10 -0300

    fix

--- src/main/java/ufes/cmp/analisadorlexico/Principal.java
+++ src/main/java/ufes/cmp/analisadorlexico/Principal.java
@@ -1,8 +1,10 @@
 package ufes.cmp.analisadorlexico;
 
+import ufes.cmp.analisadorlexico.presenter.PrincipalPresenter;
+
 public class Principal {
 
     public static void main(String[] args) {
-
+        PrincipalPresenter principal = new PrincipalPresenter();
     }
 }
