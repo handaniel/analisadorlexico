9816ace6ca9a297fedb2a8ef5a1c6788ad09aaa2
handaniel <danielhandsantiago@gmail.com>
2022-01-26 14:38:40 -0300

    Added actionlisteners

--- src/main/java/ufes/cmp/analisadorlexico/presenter/PrincipalPresenter.java
+++ src/main/java/ufes/cmp/analisadorlexico/presenter/PrincipalPresenter.java
@@ -1,24 +1,53 @@
 package ufes.cmp.analisadorlexico.presenter;
 
 import java.awt.event.ActionEvent;
+import javax.swing.table.DefaultTableModel;
 import ufes.cmp.analisadorlexico.view.PrincipalView;
 
 public class PrincipalPresenter {
 
     private PrincipalView view;
+    private DefaultTableModel tmAnalise;
+    private DefaultTableModel tmSaidas;
 
     public PrincipalPresenter() {
         view = new PrincipalView();
         view.setVisible(true);
 
+        tmAnalise = new DefaultTableModel(
+                new Object[][]{},
+                new String[]{"ID", "Linha", "Lexema", "Token"}
+        );
+
+        view.getTblAnaliseLexica().setModel(tmAnalise);
+
+        tmSaidas = new DefaultTableModel(
+                new Object[][]{},
+                new String[]{"Erro", "Linha", "Posição", "ID do Token"}
+        );
+
+        view.getTblSaidas().setModel(tmSaidas);
+
         view.getBtnCompilar().addActionListener((ActionEvent ae) -> {
             compilar();
+        });
+
+        view.getCkbExecucaoTempoReal().addActionListener((ActionEvent ae) -> {
+            compilarEmTempoReal();
         });
 
     }
 
     private void compilar() {
         System.out.println("Falta implementar!");
+    }
+
+    private void compilarEmTempoReal() {
+        if (view.getCkbExecucaoTempoReal().isSelected()) {
+            System.out.println("Ativado");
+        } else {
+            System.out.println("Desativado");
+        }
     }
 
 }
