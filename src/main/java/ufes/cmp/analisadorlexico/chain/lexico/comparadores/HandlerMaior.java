bda4a86eb4e7943bfdad3df3175564dfa6ee5a51
Cleiton Gaioti <cleiton.s.gaioti@gmail.com>
2022-01-29 12:53:17 -0300

    comparadores

---
+++ src/main/java/ufes/cmp/analisadorlexico/chain/lexico/comparadores/HandlerMaior.java
@@ -0,0 +1,33 @@
+package ufes.cmp.analisadorlexico.chain.lexico.comparadores;
+
+import ufes.cmp.analisadorlexico.chain.AbstractHandler;
+import ufes.cmp.analisadorlexico.model.Token;
+import ufes.cmp.analisadorlexico.utils.StringUtils;
+
+public class HandlerMaior extends AbstractHandler {
+
+    public HandlerMaior(Token token) {
+        super(token);
+    }
+
+    @Override
+    public void executar(Token token) {
+        if (token.getSimbolo().toString().toLowerCase().equals(">")) {
+            token.setCategoria("operador_comparacao_maior");
+        } else {
+            this.setProximo(new HandlerMaiorIgual(token));
+        }
+    }
+
+    @Override
+    public String recuperarErro(Token token) {
+        if (StringUtils.similarity(token.getSimbolo(), ">") >= 0.8) {
+            return "Esse token pode ser substituido por: operador_comparacao_maior";
+        } else if(StringUtils.similarity(token.getSimbolo(), ">") > 0.5) {
+            return "Esse token tem similaridade com: operador_comparacao_maior; " + next.recuperarErro(token);
+        }
+
+        return next.recuperarErro(token);
+    }
+
+}
