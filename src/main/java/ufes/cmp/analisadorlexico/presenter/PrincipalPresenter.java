a68716d45a3b453361a12e98a3b9a11f636e844b
handaniel <danielhandsantiago@gmail.com>
2022-01-29 10:02:28 -0300

    added presenter stuff

--- src/main/java/ufes/cmp/analisadorlexico/presenter/PrincipalPresenter.java
+++ src/main/java/ufes/cmp/analisadorlexico/presenter/PrincipalPresenter.java
@@ -1,7 +1,12 @@
 package ufes.cmp.analisadorlexico.presenter;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
+import ufes.cmp.analisadorlexico.model.Erros;
+import ufes.cmp.analisadorlexico.model.LinhaCodigo;
+import ufes.cmp.analisadorlexico.model.Token;
 import ufes.cmp.analisadorlexico.view.PrincipalView;
 
 public class PrincipalPresenter {
@@ -9,6 +14,10 @@ public class PrincipalPresenter {
     private PrincipalView view;
     private DefaultTableModel tmAnalise;
     private DefaultTableModel tmSaidas;
+    private ArrayList<Token> tokens;
+    private KeyListener keyListener;
+    private Erros erros;
+    private ArrayList<LinhaCodigo> linhas;
 
     public PrincipalPresenter() {
         view = new PrincipalView();
