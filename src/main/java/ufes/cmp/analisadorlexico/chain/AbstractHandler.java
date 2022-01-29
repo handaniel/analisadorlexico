dd0e793ca97b8ac082b2b7e185af6af40bcca7a6
handaniel <danielhandsantiago@gmail.com>
2022-01-29 10:14:23 -0300

    Ajustes

--- src/main/java/ufes/cmp/analisadorlexico/chain/AbstractHandler.java
+++ src/main/java/ufes/cmp/analisadorlexico/chain/AbstractHandler.java
@@ -1,14 +1,26 @@
 package ufes.cmp.analisadorlexico.chain;
 
+import ufes.cmp.analisadorlexico.model.Token;
+
 public abstract class AbstractHandler {
 
     protected AbstractHandler next;
     protected static int tamanhoMaxID = 15;
 
+    public AbstractHandler(Token token) {
+        executar(token);
+    }
+
     public AbstractHandler(AbstractHandler next) {
         this.next = next;
     }
-    
-    
+
+    public abstract void executar(Token token);
+
+    public abstract String recuperarErro(Token token);
+
+    public static void setTamanhoMaxID(int tamanhoMaxID) {
+        AbstractHandler.tamanhoMaxID = tamanhoMaxID;
+    }
 
 }
