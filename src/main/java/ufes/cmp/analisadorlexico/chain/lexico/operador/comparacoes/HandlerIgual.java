cc614d9145dd4d99310761c5cee0c150654e67d8
Mário Ribeiro <mario.ribeiro.88@edu.ufes.br>
2022-01-29 16:20:02 -0300

    operador de comparacao e logico

---
+++ src/main/java/ufes/cmp/analisadorlexico/chain/lexico/operador/comparacoes/HandlerIgual.java
@@ -0,0 +1,34 @@
+package ufes.cmp.analisadorlexico.chain.lexico.operador.comparacoes;
+
+import ufes.cmp.analisadorlexico.chain.AbstractHandler;
+import ufes.cmp.analisadorlexico.model.Token;
+import ufes.cmp.analisadorlexico.utils.StringUtils;
+
+public class HandlerIgual extends AbstractHandler{
+    
+    public HandlerIgual(Token token){
+        super(token);
+    }
+
+    @Override
+    public void executar(Token token) {
+       if (token.getSimbolo().toString().toLowerCase().compareTo("=") == 0){
+            token.setCategoria("operador_comparacao_igual");
+        } else {
+            this.setProximo(new HandlerIn(token));
+        }
+    }
+
+    @Override
+    public String recuperarErro(Token token) {
+        if(StringUtils.similarity(token.getSimbolo(), "=") >= 0.8 ){
+            return "Esse token pode ser substituido por: operador_comparacao_igual ";
+        } else if(StringUtils.similarity(token.getSimbolo(), "=") > 0.5 ){
+            return "Esse token tem similaridade com: operador_comparacao_igual; "+ next.recuperarErro(token);
+        } 
+        
+        return next.recuperarErro(token);
+    }
+
+    
+}
