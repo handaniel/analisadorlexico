e6d05a0ce835725f2c715646aa92c1578a2d7694
handaniel <danielhandsantiago@gmail.com>
2022-01-29 11:00:10 -0300

    Handlers tipo

--- src/main/java/ufes/cmp/analisadorlexico/chain/lexico/tipo/HandlerINTEGER.java
+++ src/main/java/ufes/cmp/analisadorlexico/chain/lexico/tipo/HandlerINTEGER.java
@@ -23,8 +23,8 @@ public class HandlerINTEGER extends AbstractHandler {
     public String recuperarErro(Token token) {
         if(StringUtils.similarity(token.getSimbolo(), "integer") >= 0.8 ){
             return "Esse token pode ser substituido por: Especificador_INTEGER ";
-        } else if(StringUtils.similarity(token.getSimbolo(), "char") > 0.5 ){
-            return "Esse token tem similaridade com: Especificador_CHAR; "+ next.recuperarErro(token);
+        } else if(StringUtils.similarity(token.getSimbolo(), "integer") > 0.5 ){
+            return "Esse token tem similaridade com: Especificador_INTEGER; "+ next.recuperarErro(token);
         } 
         
         return next.recuperarErro(token);
