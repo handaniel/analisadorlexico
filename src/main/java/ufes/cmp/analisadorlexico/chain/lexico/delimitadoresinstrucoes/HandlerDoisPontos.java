2bfaf8f8cf8e3af98316e400255ae7851de92774
handaniel <danielhandsantiago@gmail.com>
2022-01-30 19:25:02 -0300

    added ponto e virgula handler

--- src/main/java/ufes/cmp/analisadorlexico/chain/lexico/delimitadoresinstrucoes/HandlerDoisPontos.java
+++ src/main/java/ufes/cmp/analisadorlexico/chain/lexico/delimitadoresinstrucoes/HandlerDoisPontos.java
@@ -1,33 +1,35 @@
 package ufes.cmp.analisadorlexico.chain.lexico.delimitadoresinstrucoes;
 
 import ufes.cmp.analisadorlexico.chain.AbstractHandler;
+import ufes.cmp.analisadorlexico.chain.lexico.especificadores.HandlerConst;
 import ufes.cmp.analisadorlexico.model.Token;
 import ufes.cmp.analisadorlexico.utils.StringUtils;
 
-public class HandlerPontoEVirgula extends AbstractHandler {
+public class HandlerDoisPontos extends AbstractHandler {
 
-    public HandlerPontoEVirgula(Token token) {
+    public HandlerDoisPontos(Token token) {
         super(token);
     }
 
     @Override
     public void executar(Token token) {
-        if (token.getSimbolo().toLowerCase().equals(";")) {
-            token.setCategoria("Delimitador_instrucoes_pontoevirgula");
+        if (token.getSimbolo().toLowerCase().equals(":")) {
+            token.setCategoria("Delimitador_dois_pontos");
         } else {
-            this.setProximo(new HandlerPontoEVirgula(token));
+            this.setProximo(new HandlerConst(token));
         }
+        
     }
 
     @Override
     public String recuperarErro(Token token) {
         if (StringUtils.similarity(token.getSimbolo(), ";") >= 0.8) {
-            return "Esse token pode ser substituido por: Delimitador_instrucoes_pontoevirgula ";
+            return "Esse token pode ser substituido por: Delimitador_dois_pontos ";
         } else if (StringUtils.similarity(token.getSimbolo(), ";") > 0.5) {
-            return "Esse token tem similaridade com: Delimitador_instrucoes_pontoevirgula " + next.recuperarErro(token);
+            return "Esse token tem similaridade com: Delimitador_dois_pontos " + next.recuperarErro(token);
         }
 
         return next.recuperarErro(token);
     }
-
+    
 }
